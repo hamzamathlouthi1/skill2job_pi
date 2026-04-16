dc579c05392cc8216d8a13764276ded6f1af6b3b
mayssaklibi-group <mayssaklibi56@gmail.com>
2026-04-16 03:27:48 +0100

    changes

---
+++ Back-Skill2job/GestionExams/src/test/java/tn/esprit/gestionuser/GestionUserApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "eureka.client.enabled=false")
+class GestionExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
