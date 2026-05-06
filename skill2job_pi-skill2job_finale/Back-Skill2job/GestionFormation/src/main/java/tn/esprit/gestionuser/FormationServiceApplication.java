55e97ffd0fe1fb350e18a413a7c74f886421ac4f
souhakhl <souha.khelifi@esprit.tn>
2026-05-06 18:59:45 +0100

    Add empty line before main method

--- skill2job_pi-skill2job_finale/Back-Skill2job/GestionFormation/src/main/java/tn/esprit/gestionuser/FormationServiceApplication.java
+++ skill2job_pi-skill2job_finale/Back-Skill2job/GestionFormation/src/main/java/tn/esprit/gestionuser/FormationServiceApplication.java
@@ -16,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients
 public class FormationServiceApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(FormationServiceApplication.class, args);
     }
