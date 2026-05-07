c0bf8d13ce60b17050874af814818b3ee29a4d30
hamzamathlouthi1 <131817751+hamzamathlouthi1@users.noreply.github.com>
2026-05-07 13:12:23 +0100

    Fix import statement for FormationDTO

--- skill2job_pi-skill2job_finale/Back-Skill2job/GestionSession/src/main/java/tn/esprit/gestionsession/clients/FormationClient.java
+++ skill2job_pi-skill2job_finale/Back-Skill2job/GestionSession/src/main/java/tn/esprit/gestionsession/clients/FormationClient.java
@@ -3,7 +3,7 @@ package tn.esprit.gestionsession.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import tn.esprit.gestionsession.dto.FormationDTO;
+import tn.esprit.gestionsession.dto.FormationDTO; 
 
 import java.util.List;
 
